67fc89e7b28535921809a00d7decbb8fcf65f75d
bella-cockrell <heybellac@gmail.com>
2020-06-23 11:34:57 +0100

    Fixing dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-const API_KEY = 'uy2CVnMPW73mebP4g9UNEreDyu5hcDNNJeDQSk9K';
-const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+// const API_KEY = 'uy2CVnMPW73mebP4g9UNEreDyu5hcDNNJeDQSk9K';
+// const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
 
 function App() {
   return (
     <div>
-
+      Hello world
     </div>
 
   );
