c7ffdd1cfc4f9d7d5da0928de7b54578423ee0ae
bella-cockrell <heybellac@gmail.com>
2020-06-23 12:57:36 +0100

    Fetching API data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
 
-// const API_KEY = 'uy2CVnMPW73mebP4g9UNEreDyu5hcDNNJeDQSk9K';
-// const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+const API_KEY = 'uy2CVnMPW73mebP4g9UNEreDyu5hcDNNJeDQSk9K';
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
+const getWeather = () => {
+  fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+      const {
+        sol_keys,
+        validity_checks,
+        ...solData
+      } = data
+      console.log(solData);
+    })
+}
+
+getWeather();
 
 function App() {
   return (
     <div>
-      Hello world
+
     </div>
 
   );
