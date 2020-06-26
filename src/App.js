41a9aaf38cf266b81a3fdc74b4305d8b0fc734f7
bella-cockrell <heybellac@gmail.com>
2020-06-26 14:45:04 +0100

    Adding structured content and relevant icons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import getWeather from './Components/getWeather';
 import Video from './Components/Video';
 
+import thermometer from './Media/thermometer.svg';
+import windCardinal from './Media/windCardinal.svg';
+import button from './Media/button.svg';
+
+
 let selectedSolIndex;
 
 getWeather().then(sols => {
@@ -17,27 +22,41 @@ const displaySelectedSol = (sols) => {
 
 function App() {
   return (
-    <div>
+    <>
       <Video />
-      LATEST WEATHER AT ELYSIUM PLANTITIA
+      <main className='mars-current-weather'>
+        <h1 className='scrolling-title'>
+          LATEST WEATHER AT ELYSIUM PLANTITIA
+        </h1>
+        <section className='current-date'>
+          <button className='btn__current-date--left'><img src={button} alt='button' /></button>
+          <h2 className='current-date__mars'>
+            Sol 377
+          </h2>
+          <div className='current-date__earth'>
+            December 18
+          </div>
+          <button className='btn__current-date--right'><img src={button} alt='button' /></button>
+        </section>
+        <section className='readings'>
+          <div className='readings__temperature'>
+            <img src={thermometer} alt='thermometer' />
+            <h2>Temperature</h2>
+            <p>High: -20°C</p>
+            <p>Low: -98°C</p>
+          </div>
+          <div className='readings__wind'>
+            <img src={windCardinal} alt='cardinal' />
+            <h2>Wind</h2>
+            <p>75 kph</p>
+          </div>
+        </section>
+      </main>
 
-      Sol 377
-      December 18
-
-      Temperature
-      High: -20°C
-      Low: -98°C
-
-      Wind
-      75 kph
-
-      InSight is taking daily weather measurements (temperature, wind, pressure)
-      on the surface of Mars at Elysium Planitia, a flat, smooth plain near Mars’
-      equator. This is only a part of InSight’s mission. <a href='https://mars.nasa.gov/insight/mission/overview/'>Click here</a>
-      to find out more.
-
-
-    </div>
+      <footer className='glance'>
+        At a glance
+      </footer>
+    </>
 
   );
 }
