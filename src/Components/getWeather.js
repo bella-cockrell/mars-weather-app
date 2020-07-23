051601cee5cee387c4287a98ad882caec79f3034
bella-cockrell <heybellac@gmail.com>
2020-07-23 16:37:03 +0100

    Adding API call

--- src/Components/getWeather.js
+++ src/Components/getWeather.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-const API_KEY = 'uy2CVnMPW73mebP4g9UNEreDyu5hcDNNJeDQSk9K'
+
+const API_KEY = 'DEMO_KEY'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
 const GetWeather = () => {
